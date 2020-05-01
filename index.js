707a25015942200cc067f8abee1f2a01525644de
Quinn Yackulic <qyackulic@icloud.com>
2020-04-30 19:14:35 -0700

    changed js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var today = new Date();
 var hourNow = today.getHours();
 var title;
-
+console.log(hourNow)
 if (hourNow > 18) {
     title = 'NED!';
 } else if (hourNow > 12) {
@@ -12,4 +12,22 @@ if (hourNow > 18) {
     title = 'NED LUDD!';
 }
 
-document.write(`<h3>${title}</h3>`);
+document.write(`<h1>${title}</h1>`);
+
+function HaveYou() {
+    var text;
+    var user = prompt ("Do You Know What A Luddite Is?", "Answer Here");
+        switch (user) { 
+            case "Yes":
+                text = "The Internet is suprised.  Please enjoy the website."; 
+                break;
+            case "No":
+                text = "That's fairly common.  Please enjoy the website!";
+                break;
+            default:
+                text = "You're at the right place, then!"
+        }
+        document.getElementById("demo").innerHTML = text
+        }
+
+HaveYou();
